4fc67c459296888a9b4dd69493999dc98f6a2991
Maham.J <139694482+Maham-j@users.noreply.github.com>
2023-08-20 12:07:41 +0500

    Update Rock Paper Scissors Game.py

--- Rock Paper Scissors Game.py
+++ Rock Paper Scissors Game.py
@@ -1,6 +1,6 @@
-#########################################################################
-from random import*
+from random import randint
 
+# Define the images for Rock, Paper, and Scissors
 Rock = '''   
     _______
 ---'   ____)
@@ -9,7 +9,6 @@ Rock = '''
       (____)
 ---.__(___)
 '''
-
 
 Paper = '''   
     _______
@@ -20,8 +19,6 @@ Paper = '''
 ---.__________)
 '''
 
-
-
 Scissors = '''
     _______
 ---'   ____)____
@@ -31,117 +28,35 @@ Scissors = '''
 ---.__(___)
 '''
 
+# Get user input for their choice (Rock, Paper, or Scissors)
+user = input("Enter your choice (Rock, Paper, Scissors): ")
 
-images = [Rock ,Paper, Scissors]
-user = int(input())
-if user >=3 or user <0:
-    print('you Typed an Invalid number.You loose!')
+# List of images and their corresponding choices
+images = [Rock, Paper, Scissors]
+
+# Generate a random index to simulate computer's choice
+index = randint(0, 2)
+computer_choice = images[index]
+
+# Determine the game outcome based on user's choice and computer's choice
+if user == 'Rock' and computer_choice == Scissors:
+    print(f'{Rock}\n{Scissors}', 'You Won')
+    
+elif user == 'Scissors' and computer_choice == Rock:
+    print(f'{Scissors}\n{Rock}', 'You Loose')
+    
+elif user == 'Rock' and computer_choice == Paper:
+    print(f'{Rock}\n{Paper}', 'You Loose')
+    
+elif user == 'Paper' and computer_choice == Rock:
+    print(f'{Paper}\n{Rock}', 'You Won')
+    
+elif user == 'Paper' and computer_choice == Scissors:
+    print(f'{Paper}\n{Scissors}', 'You Loose')
+    
+elif user == 'Scissors' and computer_choice == Paper:
+    print(f'{Scissors}\n{Paper}', 'You Won')
 else:
-    print(f'{images[user]}')
-    
-    computer = randint(0,2)
-    index = images[computer]
-    print(f'Computer choose {index}')
-
-    if user == 0 and computer == 1:
-        print('You Loose')
-    elif user == 1 and computer == 0:
-        print('You Won')
-    elif user == 0 and computer == 2:
-        print('You Won')
-    elif user == 2 and computer == 0:
-        print('You Loose')
-    elif user == 1 and computer == 2:
-        print('You Loose')
-    elif user == 2 and computer == 1:
-        print('You Won')
-    elif user == computer:
-        print('You draw')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#############################################################################
-from random import*
-
-Rock = '''   
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-
-
-Paper = '''   
-    _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-'''
-
-
-
-Scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-
-user = input()
-
-lists = [Rock ,Paper, Scissors]
-index = randint(0,2)
-computer = lists[index]
-
-if user == 'Rock' and computer == Scissors:
-    print(f'{Rock}\n{Scissors}','You Won')
-    
-elif user =='Scissors' and computer == Rock:
-    print(f'{Scissors}\n{Rock}','You Loose')
-    
-elif user =='Rock' and computer == Paper:
-    print(f'{Rock}\n{Paper}','You Loose')
-    
-elif user =='Paper' and computer == Rock:
-    print(f'{Paper}\n{Rock}','You Won')
-    
-elif user =='Paper' and computer == Scissors:
-    print(f'{Paper}\n{Scissors}','You Loose')
-    
-elif user =='Scissors' and computer == Paper:
-    print(f'{Scissors}\n{Paper}','You Won')
-else:
-    if user =='Scissors' and computer == Scissors:
-        print(f'{Scissors}\n{Scissors}','Game Draw')
-    elif user =='Paper' and computer == Paper:
-        print(f'{Paper}\n{Paper}','Game Draw')
-    elif user =='Rock' and computer == Rock:
-        print(f'{Rock}\n{Rock}','Game Draw')
+    # Handle the case of a draw
+    if user == computer_choice:
+        print(f'{user}\n{computer_choice}', 'Game Draw')
